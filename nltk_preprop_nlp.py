df961a232def47b7c866cc320eec053a8aa8d681
olesyamba <118986574+olesyamba@users.noreply.github.com>
2023-09-27 15:07:09 +0500

    add file

--- nltk_preprop_nlp.py
+++ nltk_preprop_nlp.py
@@ -1,29 +1,44 @@
 import nltk
-import re
-import pandas as pd
+import re  # library for regular expression operations
+import string  # for string operations
+
+from nltk.corpus import stopwords  # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer  # module for stemming
+from nltk.tokenize import TweetTokenizer  # module for tokenizing strings
+
 
 # nltk.download("stopwords")  # Set of stopwords
 # nltk.download('punkt')  # Split the text into number of sentences
 # nltk.download('wordnet')  # Lemmatize
-from nltk.corpus import stopwords
 
 
-def nltk_preprocess(text_data):
-    new_text = []
-    for i in text_data:
-        # Remove and clean
-        text = re.sub('[^a-zA-Z]', " ", i)
-        # Tokenize
-        text = nltk.word_tokenize(text, language="english")
-        # Lemmatize
-        lemmatize = nltk.WordNetLemmatizer()
-        text = [lemmatize.lemmatize(word) for word in i]
-        # Combine words
-        text = "".join(text)
-        new_text.append(text)
-    return new_text
+def nltk_preprocess(text):
+    text = re.sub(r'^RT[\s]+', '', text)  # remove old style retweet text "RT"
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text)  # remove hyperlinks
+    text = re.sub(r'#', '', text)  # Removing '#' hashtag
+    text = re.sub(r'[^a-zA-Z]', " ", text)
+    text = re.sub(r'@[A-Za-z0–9]+', '', text)  # Removing @mentions
+    # Tokenize
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+    text = tokenizer.tokenize(text)
+    stopwords_english = stopwords.words('english')  # Import the english stop words list from NLTK
+    # Lemmatize
+    lemmatize = nltk.WordNetLemmatizer()
+    text = [lemmatize.lemmatize(word) for word in text if
+            word not in stopwords_english and word not in string.punctuation]
+    # Instantiate stemming class
+    stemmer = PorterStemmer()
+    text = [stemmer.stem(word) for word in text]
+    # Combine words
+    text = " ".join(text)
+    return text
 
 
 example = ['This is an example sentence. It contains stop words and punctuation!', 'If you read this - Give me a star!']
-preprocessed_example = nltk_preprocess(example)
+preprocessed_example = [nltk_preprocess(sentence) for sentence in example]
 print(preprocessed_example)
+
+tweet = 'My beautiful sunflowers on a sunny Friday morning off :) ' \
+        '#sunflowers #favourites #happy #Friday off… https://t.co/3tfYom0N1i '  # example tweet
+print('Example of preprocessing tweets \n' + 'Before: ' + tweet + '\nAfter: ' + nltk_preprocess(tweet))
